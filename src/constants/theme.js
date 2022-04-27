19d31e82b4970d6ca69cec4ebcec1f8c7546eabd
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2022-04-27 23:07:35 +0300

    Make styles sidebar

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    topBgColor: "#F4F6F6",
+    inputBgColor: "#FBFCFC",
+    borderColor: "#707B7C",
+    borderInptColor: "#D5DBDB",
+    placeholderColor: "#95A5A6",
+  },
+  fontFamily: "Mulish",
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./constants/theme";
 import { App } from "components/App/App";
 
 import "./index.css";
@@ -7,6 +9,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
